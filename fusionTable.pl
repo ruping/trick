07dd11b686a83d1e7d7629739a86a75ad029eaec
ruping <rs3412@c2b2.columbia.edu>
2015-01-26 17:02:41 -0500

    fusion

--- fusionTable.pl
+++ fusionTable.pl
@@ -54,19 +54,18 @@ foreach my $sample (@need) {
     chomp;
     my ($fusion1, $fusion2, $bp1, $bp2, $dir, $rep, $sc, $type, $strand, $cov1, $cov2, $cov3, $cov4, $cov5, $transcripts) = split /\t/;
 
-    next if ($rep =~ /R/ and $sc =~ /C/);                     #skip both rep and sc
-    next if ($fusion1 =~ /KCNMB2/ and $fusion2 =~ /KCNMB2/);  #skip KCNMB2
-    next if ($fusion1 =~ /TMPRSS3/ or $fusion2 =~ /TMPRSS3/); #skip TMPRSS3
-    next if ($cov5 < 1);                                      #skip low spanning reads
-
     my $gene1 = '';
     my $gene2 = '';
     if ($fusion1 =~ /^\S+\((.+?)\)$/){
       $gene1 = $1;
+    } else {
+      $gene1 = $fusion1;
     }
 
     if ($fusion2 =~ /\S+\((.+?)\)/){
       $gene2 = $1;
+    } else {
+      $gene2 = $fusion2;
     }
 
     #for LRIG1#########################################
@@ -85,6 +84,17 @@ foreach my $sample (@need) {
       }
     }
     ###################################################
+
+    next if ($rep =~ /R/ and $sc =~ /C/);                     #skip both rep and sc
+    next if ($fusion1 =~ /KCNMB2/ and $fusion2 =~ /KCNMB2/);  #skip KCNMB2
+    next if ($fusion1 =~ /TMPRSS3/ or $fusion2 =~ /TMPRSS3/); #skip TMPRSS3
+    next if ($cov5 < 1 or $cov3 < 2);                         #skip low spanning reads
+    next if ($cov5 < 2 and $sc eq 'CC');                      #skip low spanning reads
+    next if ($cov4/$cov5 >= 10 and $cov5 <= 3);               #high duplication
+    next if ($cov4/$cov5 >= 20 and $cov5 < 10);               #high duplication
+    next if ((($gene2 ne 'IGR' and $gene1 =~ /^$gene2/) or ($gene1 ne 'IGR' and $gene2 =~ /^$gene1/)) and $sc eq 'CC');  #remaining family
+    next if (($gene1 =~ /^IGK[JV]/ or $gene2 =~ /^IGK[JV]/) and $sc eq 'CC');                                            #IGG
+    next if (($gene1 eq 'IGR' and $gene2 =~ /^RP\d+\-\d+/) or ($gene2 eq 'IGR' and $gene1 =~ /^RP\d+\-\d+/));            #lnRNA IGR junk
 
     if ($gene1 ne ""){
       $fusions{$gene1}{$sampleDNA} = $cov5;
