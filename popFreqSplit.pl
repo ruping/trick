ca68d33091578633d8d80c9e526069d7aba25054
ruping <rs3412@c2b2.columbia.edu>
2015-01-27 22:00:49 -0500

    freq

--- popFreqSplit.pl
+++ popFreqSplit.pl
@@ -3,6 +3,9 @@ use List::Util qw(first max maxstr min minstr reduce shuffle sum);
 use Data::Dumper;
 
 my $file = shift;
+my $type = shift;  #snv or indel
+
+if ($type eq ''){$type = 'snv'};
 
 my %colnames;
 
@@ -21,12 +24,39 @@ while ( <IN> ){
 
   else {
     #now it is the real stuff
+    my $ref = $cols[$colnames{'ref'}];
+    my $alt = $cols[$colnames{'alt'}];
     my $freq = -1;
     if ($cols[$colnames{'clinical'}] =~ /\;CAF\=\[([\d\.\,]+)\]\;/){
-      my @freqs = split (/,/, $1);
-      shift @freqs;
-      #print STDERR Dumper(\@freqs);
-      $freq = max(@freqs);
+      my @freqs = split (/\,/, $1);
+
+      $cols[$colnames{'clinical'}] =~ /REFALT\=([ACGT\-\,]+)$/;
+      my @alleles = split(/\,/, $1);
+      my $index = -1;
+      my $minlengthdiff = 1000;
+
+      for (my $i = 0; $i <= $#alleles; $i++){
+        if ($type eq 'snv'){
+          if ($alleles[$i] eq $alt){
+            $index = $i;
+            last;
+          }
+        } elsif ($type eq 'indel') {
+          my $lengthdiff = abs(length($alt) - length($alleles[$i]));
+          if ($lengthdiff <= $minlengthdiff){
+            $index = $i;
+          } else {
+            next;
+          }
+        }
+      } #find index
+
+      if ($index != -1){
+        $freq = $freqs[$index];
+      } else {
+        shift @freqs;
+        $freq = max(@freqs);
+      }
     } elsif ($cols[$colnames{'id'}] =~ /^[\dKGESP]+\=([\d\.]+)$/){
       $freq = $1;
     }
