ebfc84f85decfc78897a24d1512f7ea8ae1350bc
ruping <rs3412@c2b2.columbia.edu>
2015-02-04 10:17:18 -0500

    filter

---
+++ tmpfilter2.pl
@@ -0,0 +1,37 @@
+use strict;
+
+my $file = shift;
+
+#P-values for strand bias, baseQ bias, mapQ bias and tail distance bias
+
+open IN, "$file";
+my $call;
+while  ( <IN> ){
+  chomp;
+  if ($_ =~ /^###/){
+    my @cols = split /\t/;
+    $call = $cols[1]."\t".$cols[2];
+  } else {
+    my @cols = split /\t/;
+    $cols[7] =~ /\;PV4\=(.+?)\,(.+?)\,(.+?)\,(.+?);/;
+    my $strandb = $1;
+    my $baseqb = $2;
+    my $mapqb = $3;
+    my $tailb = $4;
+    my @bias;
+    push (@bias, $strandb);
+    push (@bias, $baseqb);
+    push (@bias, $mapqb);
+    push (@bias, $tailb);
+    my $bad = 0;
+    foreach my $bias (@bias) {
+      if ($bias =~ /e/) {
+        $bad = 1;
+      } elsif ($bias <= 0.05) {
+        $bad = 1;
+      }
+    }
+    print "$call\t$bad\n";
+  }
+}
+close IN;
