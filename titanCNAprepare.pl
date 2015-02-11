0209efc64c9d55bec5ca54c6b4841f5452774ce3
ruping <rs3412@c2b2.columbia.edu>
2015-02-11 15:20:42 -0500

    titan

--- titanCNAprepare.pl
+++ titanCNAprepare.pl
@@ -5,6 +5,8 @@ use strict;
 #print "$int\n";
 my $blood = shift;
 my $tumor = shift;
+my 
+
 
 open IN, "$blood";
 while ( <IN> ){
@@ -15,7 +17,7 @@ while ( <IN> ){
   }
   my ($chr, $pos, $id, $ref, $alt, $bloodmaf, $bloodd, $cmeanav, $cmedianav) = split /\t/;
   my $vard = &round($bloodmaf*$bloodd);
-  next if ($cmeanav > 3 and $cmedianav > 3);
+  next if ($cmeanav >= 3 and $cmedianav >= 3);
   if ($bloodd >= 7 and $bloodmaf >= 0.25 and $bloodmaf <= 0.75){
     print "$_\n";
   }
