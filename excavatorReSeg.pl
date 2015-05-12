b5f996fe57d2f27f45e26159ec2a0e7a49acb79c
ruping <rs3412@c2b2.columbia.edu>
2015-05-12 12:36:27 -0400

    ex

--- excavatorReSeg.pl
+++ excavatorReSeg.pl
@@ -7,10 +7,12 @@ my $dir = shift;
 my $prefix = shift;
 
 my @files = bsd_glob("$dir/$prefix*/FastCallResults_*.txt");
-print STDERR Dumper (\@files);
-
+#print STDERR Dumper (\@files);
 
 foreach my $file (@files) {
-
-  
+  my $sample;
+  if ($file =~ /$dir\/($prefix\d+)\/FastCallResults_$prefix\d+\.txt/){
+    $sample = $1;
+  }
+  print STDERR "$sample\n";
 }
