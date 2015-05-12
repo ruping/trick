51807bcd33f6c8de53619e2380d367c793c853b6
ruping <rs3412@c2b2.columbia.edu>
2015-05-12 13:58:34 -0400

    ex

--- excavatorReSeg.pl
+++ excavatorReSeg.pl
@@ -9,10 +9,26 @@ my $prefix = shift;
 my @files = bsd_glob("$dir/$prefix*/FastCallResults_*.txt");
 #print STDERR Dumper (\@files);
 
+print "#FID\t$CHR\tBP1\tBP2\tTYPE\n";
+
 foreach my $file (@files) {
   my $sample;
   if ($file =~ /$dir\/($prefix\d+)\/FastCallResults_$prefix\d+\.txt/){
     $sample = $1;
+  } else {
+    print STDERR "$file is strange!!!\n";
+    next;
   }
-  print STDERR "$sample\n";
+  #print STDERR "$sample\n";
+
+  open IN, "$file";
+  while ( <IN> ){
+    chomp;
+    next if /^Chromosome/;
+    my ($chr, $start, $end, $logR, $CN, $Call, $ProbCall) = split /\t/;
+    printf("%s\n", join($sample,$chr,$start,$end,$CN));
+  }
+  close IN;
 }
+
+exit 0;
