081debf080cd714e99e97e7a79a7f85676ccb1c7
ruping <rs3412@c2b2.columbia.edu>
2015-05-26 14:14:35 -0400

    threshold

--- recurrency.pl
+++ recurrency.pl
@@ -8,6 +8,7 @@ my $somaticInfo = shift; #if for somatic judgement
 my %somatic;
 my %germline;
 if ($somaticInfo ne '' and -s "$somaticInfo") {
+
   open IN, "$somaticInfo";
   while ( <IN> ){
     chomp;
@@ -197,7 +198,7 @@ while ( <IN> ) {
           $stype = 'good';
           $soma = ($soma eq 'NA')? $tumorSamp."\[$stype\]".',':$soma.$tumorSamp."\[$stype\]".',';
         } elsif (exists($blood{$tumorSamp})) {
-          if ($blood{$tumorSamp} < 0.1 and $tumor{$tumorSamp}/$blood{$tumorSamp} >= 4) {
+          if ($blood{$tumorSamp} <= 0.1 and $tumor{$tumorSamp}/$blood{$tumorSamp} >= 3) {
             $stype = 'doubt';
             $soma = ($soma eq 'NA')? $tumorSamp."\[$stype\]".',':$soma.$tumorSamp."\[$stype\]".',';
           } elsif (exists($unknown{$tumorSamp})) {
