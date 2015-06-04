8e786531d4b03a870eec93f7e7c07226050d075d
ruping <rs3412@c2b2.columbia.edu>
2015-06-04 19:49:00 -0400

    final

--- snvFreqProcessing.pl
+++ snvFreqProcessing.pl
@@ -16,6 +16,7 @@ while ( <IN> ) {
     my $dbsnp = $cols[$colindex{'dbsnp142.snv.vcf.INFO.CAF'}];
     my $somatic = $cols[$colindex{'somatic'}];
     my $founds = $cols[$colindex{'founds'}];
+    my $espTac = $cols[$colindex{'ESP6500SI.snv.vcf.INFO.TAC'}];
     my $freq = 'NA';
 
     my $keep = 0;
@@ -47,20 +48,33 @@ while ( <IN> ) {
 
     # known ESP rare ones
     my $freqESP = 22;
-    if ($freqDBsnp == 22 and $esp ne 'NA') {
+    if ($freqDBsnp == 22 and $esp ne 'NA' and $espTac ne '') {
       my @espf = split(",", $esp);
-      $freqESP = $espf[$#espf] * 0.01;
+      my @espTac = split(",", $espTac);  #in the format of alt, ref
+      if ($espTac[0] < $espTac[1]) {       #alt should be minor allele
+        $freqESP = $espf[$#espf] * 0.01;
+      }
       if ($freqESP < 0.005) {
         $keep = 1;
       }
     }
 
+    my $freqESP5400 = 22;
+    if ($freqDBsnp == 22 and $freqESP == 22 and $id =~ /ESP5400=(.+?)/) {
+      $freqESP5400 = $1;
+      if ($freqESP5400 < 0.005) {
+        $keep = 1;
+      }
+    }
+
+
     if ($freqDBsnp != 22) {
       $freq = $freqDBsnp;
+    } elsif ($freqESP != 22) {
+      $freq = $freqESP;
+    } elsif ($freqESP5400 != 22){
+      $freq = $freqESP5400;
     }
-    #elsif ($freqESP != 22) {
-    #  $freq = $freqESP;
-    #}
 
     if ($keep == 1){
       print "$_\t$freq\n";
