9709a94bd7f4a0f2693d60eb81b81ce25d2d7f0b
ruping <rs3412@c2b2.columbia.edu>
2015-06-12 10:49:35 -0400

    final filtration

--- backVarQualCheck.pl
+++ backVarQualCheck.pl
@@ -13,18 +13,25 @@ my %colnames;
 my %colindex;
 while ( <IN> ) {
   chomp;
-  if (/^[\#]?chr\t/) {
-    #it is header
+  if (/^[\#]?chr\t/) { #it is header
     my @cols = split /\t/;
     for(my $i = 0; $i <= $#cols; $i++) {
       $colindex{$cols[$i]} = $i;
       $colnames{$i} = $cols[$i];
     }
     printf("%s\n", join("\t", @cols));
-  } else {
+  } else {  #normal lines
+
     my @cols = split /\t/;
-    if ($cols[$colindex{'somatic'}] ne 'NA') { #check somatic ones
-      my $keep = 0;
+    my $colSomatic = $cols[$colindex{'somatic'}];
+    my $colGermline = $cols[$colindex{'germline'}];
+    my $colFounds = $cols[$colindex{'founds'}];
+    my $colFreq = $cols[$colindex{'freq'}];
+    my $colId = $cols[$colindex{'id'}];
+    my $colFunction = $cols[$colindex{'function'}];
+
+    if ( $colSomatic ne 'NA' ) { #check somatic ones
+      my $skeep = 0;
       my @somaticSamps = split(',', $cols[$colindex{'somatic'}]);
       foreach my $somaticSamp (@somaticSamps) {
         $somaticSamp =~ /^(.+?)\[/;
@@ -34,16 +41,40 @@ while ( <IN> ) {
           my $oriMaf = $1;
           my $oriQual = $2;
           if ( $oriQual > 40 ) {
-            $keep = 1;
+            $skeep = 1;
           } #good qual
         } #called samtools
       } #foreach somatic sample
-
-      if ($keep == 0){
+                             #### common ####     #################### possible common #################     ### only somatic ###
+      if ( $skeep == 0 and ( $colFreq > 0.005 or ($colId ne '.' and $colFreq eq 'NA' and $colFounds >= 3) or $colGermline eq 'NA' ) ) {
         next;
       }
-    } #somatic ones
-    my @recSamps = split(',', $cols[$colindex{'germline'}]);
-  }
+    } #check somatic ones
+
+    if ( $colGermline ne 'NA' ) { #check germline ones
+      my $gfounds = 0;
+      my @germSamps = split(',', $cols[$colindex{'germline'}]);
+      foreach my $gSamp (@germSamps) {
+        my $oriCall = $cols[$colindex{$gSamp}];
+        if ($oriCall =~ /^(.+?)\|(.+?)$/) {
+          my $oriMaf = $1;
+          my $oriQual = $2;
+          if ( $oriQual > 40 ) {
+            $gfounds += 1;
+          }                     #good qual
+        }                       #original call
+      }                         #check germline ones
+      if ( $colFounds >= $foundTh ) {
+        my $gfoundRatio = sprintf("%.2f", $gfounds/$colFounds);
+        if ($gfoundRatio < 0.5){
+          next;
+        }
+      }
+    }  #check germline ones
+
+    printf("%s\n", join("\t", @cols));
+
+  } #normal lines
+
 }
 close IN;
