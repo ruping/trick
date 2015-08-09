305e05fdb42c0b4bbe1e28fe94b070237a67ee54
ruping <rs3412@c2b2.columbia.edu>
2015-08-09 14:57:30 -0700

    big programe

--- recurrency.pl
+++ recurrency.pl
@@ -293,15 +293,17 @@ while ( <IN> ) {
           my $depth = $cols[$i+1];
           my $vard = sprintf("%.1f", $maf*$depth);
 
-          if (($endsratio <= 0.9 or ((1-$endsratio)*$vard >= 2)) and (($cmean+$cmedian) < 5.5)){  #true event
-            $maf = $maf;
-          } else {
-            $maf = 0;   #not reliable somatic
+          if (exists $somatic{$samp}) {     #for tumor samples require some additional thing
+            if (($endsratio <= 0.9 or ((1-$endsratio)*$vard >= 2)) and (($cmean+$cmedian) < 5.5)) { #true event
+              $maf = $maf;
+            } else {
+              $maf = 0;         #not reliable somatic
+            }
           }
 
           #print STDERR "$samp\t$maf\t$endsratio\t$cmean\t$cmedian\n";
 
-          if (exists $somatic{$samp}) { #it is tumor
+          if (exists $somatic{$samp}) {       #it is tumor
              $tumor{$samp} = $maf if ($vard >= 2 and $maf >= 0.05);
           } elsif (exists $germline{$samp}) { #it is blood
             foreach my $ct (@{$germline{$samp}}) {
