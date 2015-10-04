e57ceed16dfa2ab2e2d6265854c977858fdbac6c
ruping <rs3412@c2b2.columbia.edu>
2015-10-04 10:25:15 -0700

    split

---
+++ changeSamHeader.pl
@@ -0,0 +1,25 @@
+use strict;
+
+my $samheader = shift;
+my $chrom = shift;
+
+if ($chrom !~ /^[cC][hH][rR]/){
+  $chrom = 'chr'.$chrom;
+}
+
+open IN, "$samheader";
+while ( <IN> ){
+  chomp;
+  if ( $_ =~ /^\@SQ\tSN\:(\w+)\tLN\:\d+/ ) {
+    my $chr = $1;
+    if ($chr !~ /^[cC][hH][rR]/){
+      $chr = 'chr'.$chr;
+    }
+    if ( $chrom eq $chr ) {
+      print "$_\n";
+    }
+  } else {
+    print "$_\n";
+  }
+}
+close IN;
