c627a9d2e5c7ac63b693cfcdadd083bf07bcf115
ruping <rs3412@c2b2.columbia.edu>
2015-10-05 11:44:36 -0700

    bamlist

--- bam_list.pl
+++ bam_list.pl
@@ -12,15 +12,16 @@ $chr{'Y'} = '';
 $chr{'MT'} = '';
 
 my $root = shift;
+my $sample = shift;
 my $suffix = shift;
-my $delim = shift;
+my $delim = shift;   #by default it will generate bam files line by line
 
 
 my @bam_files = bsd_glob("$root/*\.$suffix");
 foreach my $bam_file (@bam_files){
-  $bam_file =~ /^$root\/(.+?)\.$suffix$/;
+  $bam_file =~ /^$root\/$sample\.(.+?)\.$suffix$/;
   my $chr = $1;
-  if (exists($chr{$chr})){
+  if (exists($chr{$chr})) {
     if ($delim =~ /space/){
        print "$bam_file ";
        next;
