fc5b6250f556d91f849375e5b8455c9b7324856a
ruping <rs3412@c2b2.columbia.edu>
2015-10-04 22:18:07 -0700

    split

--- runVcfconcat.pl
+++ runVcfconcat.pl
@@ -3,14 +3,20 @@ use File::Glob ':glob';
 use File::Basename;
 
 my $dir = shift;
+my $outdir = shift;
 my @vcfs = bsd_glob("$dir/*.vcf");
 
 my %vcfs;
+my $outvcfname = '';
 foreach my $vcf (@vcfs){
   my $basename = basename($vcf);
-  if ($basename =~ /^([A-Za-z0-9\-\_]+)\.([A-Za-z0-9]+)\./){
+  if ($basename =~ /^([A-Za-z0-9\-\_]+)\.([A-Za-z0-9]+)\.(.+?)$/){
     my $sample = $1;
     my $chr = $2;
+    my $fname = $3;
+    if ($outvcfname eq ''){
+      $outvcfname = $sample.'.'.$fname;
+    }
     $vcfs{$chr} = $vcf;
   } else {
     print "strang $vcf\n";
@@ -23,4 +29,17 @@ foreach my $chr (sort {$a cmp $b} keys %vcfs){
   $vcfs .= "$vcfs{$chr} ";
 }
 
-print "$vcfs\n";
+my $cmd = "vcf-concat $vcfs >$outdir/$outvcfname";
+unless (-s "$outdir/$outvcfname"){
+  RunCommand($cmd, 1, 0);
+}
+
+sub RunCommand {
+  my ($command,$noexecute,$quiet) = @_ ;
+  unless ($quiet){
+    print STDERR "$command\n\n";
+  }
+  unless ($noexecute) {
+    system($command);
+  }
+}
