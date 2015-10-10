2dbf5d2779d6f36e9ff4a0a13298057b1b48d840
ruping <rs3412@c2b2.columbia.edu>
2015-10-09 22:34:28 -0700

    subclonal

--- recurrency.pl
+++ recurrency.pl
@@ -401,17 +401,20 @@ while ( <IN> ) {
 
       my $soma = 'NA';
       my $germ = 'NA';
-      foreach my $tumorSamp (keys %tumor) {
+      foreach my $tumorSamp (keys %tumor) {   #check blood to confirm that it is somatic
         my $stype = 'NA';
         if (exists $nonblood{$tumorSamp}) {
           $stype = 'good';
+          $stype .= ($tumor{$tumorSamp} < 0.01)? 'Sub' : '';  #add subclonal info
           $soma = ($soma eq 'NA')? $tumorSamp."\[$stype\]".',':$soma.$tumorSamp."\[$stype\]".',';
         } elsif (exists($blood{$tumorSamp})) {
           if ($blood{$tumorSamp} < 0.02 and $tumor{$tumorSamp}/$blood{$tumorSamp} >= 4) {
             $stype = 'doubt';
+            $stype .= ($tumor{$tumorSamp} < 0.01)? 'Sub' : '';  #add subclonal info
             $soma = ($soma eq 'NA')? $tumorSamp."\[$stype\]".',':$soma.$tumorSamp."\[$stype\]".',';
           } elsif (exists($unknown{$tumorSamp})) {
             $stype = 'undef';
+            $stype .= ($tumor{$tumorSamp} < 0.01)? 'Sub' : '';  #add subclonal info
             $soma = ($soma eq 'NA')? $tumorSamp."\[$stype\]".',':$soma.$tumorSamp."\[$stype\]".',';
           } else {
             $germ = ($germ eq 'NA')? $tumorSamp.',':$germ.$tumorSamp.',';
