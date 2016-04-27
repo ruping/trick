8d05970868bd67f6442b9316645dc8dc60ee41d4
ruping <rs3412@c2b2.columbia.edu>
2016-04-27 11:46:07 -0700

    mu2

--- spruceInputPrepare.pl
+++ spruceInputPrepare.pl
@@ -16,8 +16,8 @@ while ( <IN> ){
     $header = '#'.$header;
     $print .= $header."\n";
   } else {
-    my ($sample_index, $sample_label, $character_label, $character_index, $vaf_lb, $vaf_mean, $vaf_ub, $x1, $y1, $mu1, $seg) = split /\t/;
-    my $buffer = join("\t",$sample_index, $sample_label, $character_label, $character_index, $vaf_lb, $vaf_mean, $vaf_ub, $x1, $y1, $mu1);
+    my ($sample_index, $sample_label, $character_label, $character_index, $vaf_lb, $vaf_mean, $vaf_ub, $x1, $y1, $mu1, $x2, $y2, $mu2, $seg) = split /\t/;
+    my $buffer = join("\t",$sample_index, $sample_label, $character_label, $character_index, $vaf_lb, $vaf_mean, $vaf_ub, $x1, $y1, $mu1, $x2, $y2, $mu2);
     $buffer =~ s/\tNA//g;
     $print .= $buffer."\n";
     push(@allsampindex, $sample_index);
