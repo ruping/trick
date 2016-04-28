efc25d3d48f77a51ccd36e3bf1cefc6b114de318
ruping <rs3412@c2b2.columbia.edu>
2016-04-27 18:39:30 -0700

    cghub

--- cghubExplore.pl
+++ cghubExplore.pl
@@ -7,6 +7,8 @@ my $study = "SRP";
 my $bamfile = "SRP";
 my $baifile = "SRP";
 my $center = "SRP";
+my $plf = "SRP";
+my $lib = "SRP";
 my %need;
 
 open IN, shift;
@@ -31,12 +33,18 @@ while ( <IN> ) {
       next if $bamfile =~ /^CCLE/;
     } elsif ($_ =~ /^\s+filename\s+\:\s+(.+?\.bai)$/) {
       $baifile = $1;
+    } elsif ($_ =~ /^\s+platform\s+\:\s+(\S+)$/){
+      $plf = $1;
+    } elsif ($_ =~ /^\s+library_strategy\s+\:\s+(\S+)$/){
+      $lib = $1;
     } elsif ($_ =~ /^\s+analysis\_data\_uri/) { #save
       #print "$sampleId\n";
       $need{$sampleId}{'aid'} = $analysisId;
       $need{$sampleId}{'bam'} = $bamfile;
       $need{$sampleId}{'bai'} = $baifile;
       $need{$sampleId}{'cen'} = $center;
+      $need{$sampleId}{'plf'} = $plf;
+      $need{$sampleId}{'lib'} = $lib;
     }
   }
 }
@@ -46,7 +54,9 @@ foreach my $sampleId (sort keys %need) {
   my $bam = $need{$sampleId}{'bam'};
   my $aid = $need{$sampleId}{'aid'};
   my $cen = $need{$sampleId}{'cen'};
-  print "$aid\t$bam\t$cen\n";
+  my $plf = $need{$sampleId}{'plf'};
+  my $lib = $need{$sampleId}{'lib'};
+  print "$sampleId\t$bam\t$cen\t$plf\t$lib\n";
 }
 
 exit 0;
