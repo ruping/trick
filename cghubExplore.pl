50d59b4c999bd314aee8dcb1820a31e8cdee7572
ruping <rs3412@c2b2.columbia.edu>
2016-04-27 18:15:29 -0700

    cghub

---
+++ cghubExplore.pl
@@ -0,0 +1,37 @@
+use strict;
+
+open IN, shift;
+my %need;
+while ( <IN> ) {
+  chomp;
+  if ($_ =~ /^\s+(Analysis\s+\d+)$/) {
+    $id = $1;
+  } else {
+    next if $id !~ /^Analysis/;
+    if ($_ =~ /^\s+legacy_sample_id\s+\:\s+(.+?)$/) {
+      $sampleId = $1;
+      next if $sampleId =~ /^CCLE/;
+    } elsif ($_ =~ /^\s+analysis_id\s+\:\s+(.+?)$/) {
+      $analysisId = $1;
+    } elsif ($_ =~ /^\s+study\s+\:\s+(.+?)$/) {
+      $study = $1;
+    } elsif ($_ =~ /^\s+filename\s+\:\s+(.+?\.bam)$/) {
+      $bamfile = $1;
+      next if $bamfile =~ /^CCLE/;
+    } elsif ($_ =~ /^\s+filename\s+\:\s+(.+?\.bai)$/) {
+      $baifile = $1;
+    } elsif ($_ =~ /^\s+analysis\_data\_uri/) { #save
+      $need{$sampleId}{'aid'} = $analysisId;
+      $need{$sampleId}{'bam'} = $bamfile;
+      $need{$sampleId}{'bai'} = $baifile;
+    }
+  }
+}
+close IN;
+
+foreach my $sampleId (sort keys %need) {
+  my $bam = $need{$sampleId}{'bam'};
+  my $aid = $need{$sampleId}{'aid'};
+}
+
+exit 0;
