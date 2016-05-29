cb1f553f158125d1af7719a7fd261d1c807a6fbc
ruping <rs3412@c2b2.columbia.edu>
2016-05-29 14:53:58 -0700

    match multi names

--- changeFileNames.pl
+++ changeFileNames.pl
@@ -1,16 +1,20 @@
 use strict;
 use File::Glob ':glob';
 use File::Basename;
+use Data::Dumper; 
 
 my $dir = shift;
 my $pattern = shift;
 my $changed = shift;
+
 my @files = bsd_glob("$dir/*$pattern*");
+print STDERR "pattern: $pattern\n";
+print STDERR "changed: $changed\n";
+print STDERR (\@files);
 
 foreach my $file (@files){
   my $basename = basename($file);
   my $dirname = dirname($file);
-  #(my $newfile = $file) =~ s/$pattern/$changed/;
   $basename =~ s/$pattern/$changed/;
   my $newfile = $dirname.'/'.$basename;
   my $cmd = "mv -f $file $newfile";
