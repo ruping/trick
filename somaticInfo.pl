242bb3fa2a0fec6c9c5c6260b7a20826028ac930
ruping <rs3412@c2b2.columbia.edu>
2016-06-30 11:21:24 -0700

    somaticInfo

---
+++ somaticInfo.pl
@@ -0,0 +1,33 @@
+use strict;
+use Data::Dumper;
+use File::Glob ':glob';
+use File::Basename;
+
+my @files = bsd_glob("./*.bam");
+my %data;
+
+foreach my $bam (@files) {
+  $bam = basename($bam);
+  $bam =~ /([A-Z0-9\-]+)\_([A-Za-z0-9]+)\.bam/;
+  my $id = $1;
+  my $type = $2;
+  if ($type =~ /Tumor/) {
+    push(@{$data{$id}{'T'}}, $bam)
+  } else {
+    push(@{$data{$id}{'N'}}, $bam)
+  }
+}
+
+#print STDERR Dumper(\%data);
+
+foreach my $id (keys %data) {
+  foreach my $tumor (@{$data{$id}{'T'}}) {
+    my @normal = @{$data{$id}{'T'}};
+    if ($#normal > 0){
+      die("more than one normal for $id");
+    }
+    print "$tumor\t$normal[0]\n";
+  }
+}
+
+exit 0;
