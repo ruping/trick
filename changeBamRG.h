3be2da0b325f08a29b7cde8f98c8690cd8216b4f
ruping <rs3412@c2b2.columbia.edu>
2016-08-06 11:06:19 -0700

    changebam

--- changeBamRG.h
+++ changeBamRG.h
@@ -5,11 +5,15 @@
 
 
 struct parameters {
-  char* region_f;
   char* mapping_f;
+  char* pileup;
   char* type;
-  unsigned int unique;
-  unsigned int chr;
+  char* writer;
+  char* unmapped;
+  char* arp;
+  char* breakpoint;
+  unsigned int readlength;
+  unsigned int maxIntron;
 };
 
 struct parameters* interface(struct parameters* param,int argc, char *argv[]);
@@ -24,40 +28,46 @@ struct parameters* interface(struct parameters* param, int argc, char *argv[]){
   int c;     // the next argument
   int help = 0;
 
-  if (argc < 2) {
+  if (argc < 2){
     usage();
     exit(0);
   }
 
   param = new struct parameters;
-  param->region_f = new char;
+  param->pileup = new char;
   param->mapping_f = new char;
   param->type = new char;
- 
+  param->writer = new char;
+  param->unmapped = new char; 
+  param->arp = new char;
+  param->breakpoint = new char;
+
   const struct option long_options[] ={
-    {"region",1,0, 'r'},
     {"mapping",1,0,'m'},
+    {"pileup",1,0,'p'},
     {"type",1,0,'t'},
-    {"unique",0,0,'u'},
-    {"chr",0,0,'c'},
+    {"writer",1,0,'w'},
+    {"unmapped",1,0,'u'},
+    {"arp",1,0,'a'},
+    {"breakpoint",1,0,'b'},
+    {"readlength",1,0,'l'},
+    {"maxIntron",1,0,'i'},
     {"help",0,0,'h'},
     {0, 0, 0, 0}
   };
 
-  while (1) {
+
+  while (1){
 
     int option_index = 0;
-    c = getopt_long_only (argc, argv,"hur:m:t:c",long_options, &option_index);
+    c = getopt_long_only (argc, argv,"hm:t:p:w:u:a:b:l:i:",long_options, &option_index);
 
-    if (c == -1) {
+    if (c == -1){
       break;
     }
 
     switch(c) {
     case 0:
-      break;
-    case 'r':
-      param->region_f = optarg;
       break;
     case 'm':
       param->mapping_f = optarg;
@@ -65,11 +75,26 @@ struct parameters* interface(struct parameters* param, int argc, char *argv[]){
     case 't':
       param->type = optarg;
       break;
-    case 'u':
-      param->unique = 1;
+    case 'p':
+      param->pileup = optarg;
       break;
-    case 'c':
-      param->chr = 1;
+    case 'w':
+      param->writer = optarg;
+      break;
+    case 'u':
+      param->unmapped = optarg;
+      break;
+    case 'a':
+      param->arp = optarg;
+      break;
+    case 'b':
+      param->breakpoint = optarg;
+      break;
+    case 'l':
+      param->readlength = atoi(optarg);
+      break;
+    case 'i':
+      param->maxIntron = atoi(optarg);
       break;
     case 'h':
       help = 1;
@@ -83,7 +108,7 @@ struct parameters* interface(struct parameters* param, int argc, char *argv[]){
     }
   }
 
-  if(help) {
+  if(help){
     usage();
     delete_param(param);
     exit(0);
@@ -94,23 +119,31 @@ struct parameters* interface(struct parameters* param, int argc, char *argv[]){
 
 void usage()
 {
-  fprintf(stdout, "\ngrep_starts, Copyright (C) 2011 Sun Ruping <ruping@molgen.mpg.de>\n");
+  fprintf(stdout, "\nchangeBamRG, Copyright (C) 2016 Sun Ruping <ruping@stanford.edu>\n");
   fprintf(stdout, "\n");
   fprintf(stdout, "Usage: %s options [inputfile] \n\n", program_name);
-  fprintf(stdout, "-h --help    print the help message\n");
-  fprintf(stdout, "-r --region  <filename>  UCSC gene annotation file in 12 column bed format (should be sorted according to chromosomes and coordinates)\n");
-  fprintf(stdout, "-m --mapping <filename>  mapping_file (RNA-seq bam file, chromosomes and coordinates sorted also)\n");
-  fprintf(stdout, "-q --unique              only calculate for uniquely mapped reads (set this when the bam files contain multi-mapping reads).\n");
-  fprintf(stdout, "-c --chr                 set when the chromosome names in bam files starting with \'chr\'.\n");
-  fprintf(stdout, "-t --type    <p/s>       under development\n");
+  fprintf(stdout, "-h --help        print the help message\n");
+  fprintf(stdout, "-m --mapping     mapping_file (bam file)\n");
+  fprintf(stdout, "-p --pileup      <forget it, currently it is no use> yes: allow pileup, no: skip redundant reads \n");
+  fprintf(stdout, "-w --writer      the bam output name (for mapped ailgnments).\n");
+  fprintf(stdout, "-u --unmapped    write the unmapped tag names into this file.\n");
+  fprintf(stdout, "-a --arp         filename of the arp read name (for fusion assembly use, default not write).\n");
+  fprintf(stdout, "-b --breakpoint  the file for output of potential breakpoint.\n");
+  fprintf(stdout, "-l --readlength  the length of the reads.\n");
+  fprintf(stdout, "-i --maxIntron   the maximum intron length (for breakpoints).\n");
+  fprintf(stdout, "-t --type        (p)aired-end or (s)ingle-end or just to (fixflag, multiMis).\n");
   fprintf(stdout, "\n");
 }
 
 
 void delete_param(struct parameters* param)
 {
-  delete(param->region_f);
   delete(param->mapping_f);
   delete(param->type);
+  delete(param->pileup);
+  delete(param->writer);
+  delete(param->unmapped);
+  delete(param->arp);
+  delete(param->breakpoint);
   delete(param);
 }
