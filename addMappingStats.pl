0faa3925cab64da0f50c6ab2326484698b9a1c60
ruping <rs3412@c2b2.columbia.edu>
2016-09-06 15:10:28 -0700

    addms

--- addMappingStats.pl
+++ addMappingStats.pl
@@ -6,22 +6,24 @@ my @mappingStats = split (',', $ms);
 
 my @stats;
 my %stats;
+my $fileCount = 1;
 foreach my $mappingStats (@mappingStats){
   open IN, "$mappingStats";
   while ( <IN> ) {
     chomp;
     $_ =~ /^(\w+\:)(\s+)(\d+)$/;
     my $statn = $1;
-    push (@stats, $statn);
+    push (@stats, $statn) if ($fileCount == 1);
     my $space = $2;
     my $count = $3;
     $stats{$statn}{'space'} = $space;
     $stats{$statn}{'count'} += $count;
   }
   close IN;
+  $fileCount ++;
 }
 
-foreach my $statn (@stats){
+foreach my $statn (@stats) {
   printf("%s\n",join("", $statn, $stats{$statn}{'space'}, $stats{$statn}{'count'}));
 }
 
