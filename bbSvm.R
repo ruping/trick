56f5dc626053cd5907d951832adff844042cffaa
ruping <rs3412@c2b2.columbia.edu>
2016-10-25 12:07:04 -0700

    bbSvm

--- bbSvm.R
+++ bbSvm.R
@@ -152,7 +152,7 @@ load("stats.merged.rda")
 
 
 features = c("fHsub","fHss","FST","KSD","rAUC")
-data = stats.merged4
+data = stats.merged8
 colnames = colnames(data)
 seeds = 1943:1962         #20 times each
 seeds = as.numeric(seeds)
